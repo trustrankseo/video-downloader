2c82a06954de729c47e7ded4dcd8b10679e2c3df
trustrankseo <a627377372@gmail.com>
2026-09-07 08:32:17 -0700

    Remove aria2c from lean APK

--- android-v2/app/src/main/java/com/faisal/freshdownloader/DownloaderApp.kt
+++ android-v2/app/src/main/java/com/faisal/freshdownloader/DownloaderApp.kt
@@ -1,7 +1,6 @@
 package com.faisal.freshdownloader
 
 import android.app.Application
-import com.yausername.aria2c.Aria2c
 import com.yausername.ffmpeg.FFmpeg
 import com.yausername.youtubedl_android.YoutubeDL
 import com.yausername.youtubedl_android.YoutubeDLException
@@ -12,7 +11,6 @@ class DownloaderApp : Application() {
         try {
             YoutubeDL.getInstance().init(this)
             FFmpeg.getInstance().init(this)
-            Aria2c.getInstance().init(this)
         } catch (e: YoutubeDLException) {
             e.printStackTrace()
         }
