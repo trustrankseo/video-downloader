7159527e35387bc6651689c0c289406652c049b4
trustrankseo <a627377372@gmail.com>
2026-09-07 11:03:26 -0700

    Bump Universal Downloader to v1.2.1 Facebook hotfix

--- android-v2/app/build.gradle.kts
+++ android-v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.faisal.freshdownloader"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
