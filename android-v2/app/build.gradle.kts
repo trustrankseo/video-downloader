7b501633cca207b5158ca3548cb329be6912babf
trustrankseo <a627377372@gmail.com>
2026-09-08 04:13:38 +0500

    Bump Android app to v1.3.6

--- android-v2/app/build.gradle.kts
+++ android-v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.faisal.freshdownloader"
         minSdk = 29
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.3.5"
+        versionCode = 20
+        versionName = "1.3.6"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
