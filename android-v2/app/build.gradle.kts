eacd8bbf70520bf77812271b4b1b16d57e01efac
trustrankseo <a627377372@gmail.com>
2026-09-07 13:50:54 -0700

    Bump Universal Downloader to v1.3.2

--- android-v2/app/build.gradle.kts
+++ android-v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.faisal.freshdownloader"
         minSdk = 29
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.3.1"
+        versionCode = 16
+        versionName = "1.3.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
